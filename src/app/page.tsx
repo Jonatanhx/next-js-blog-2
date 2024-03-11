6b104548dd2aaec898ae99c3b1098cdfa82c6dbc
Jonatan Helander <jonatanhelander@hotmail.com>
2024-03-11 11:30:07 +0100

    Add links to blogposts and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
 import fs from "fs/promises";
+import Link from "next/link";
 export default async function home() {
   const data = await fs.readFile("data/blog-posts.json", { encoding: "utf-8" });
 
   const blogPosts = JSON.parse(data);
 
-  console.log(blogPosts);
+  const date = new Date().toUTCString();
 
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-8xl font-bold mt-8 mb-12 border-b-2 border-white">
+      <h1 className="text-8xl font-bold mt-8 mb-4 border-b-2 border-white">
         Blog
       </h1>
-
+      <span className="mb-12">{date}</span>
       <h2 className="text-4xl mb-4">Latest posts</h2>
       <ul className="flex flex-col gap-4">
         {blogPosts.map((post: any) => (
-          <li key={post.id} className="border p-4 hover: cursor-pointer">
+          <Link
+            href={`/posts/${post.slug}`}
+            key={post.id}
+            className="border p-4"
+          >
             <h3 className="text-4xl font-thin">{post.title}</h3>
             <span className="text-gray-300">{post.author}</span>
-          </li>
+          </Link>
         ))}
       </ul>
     </main>
---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import fs from "fs/promises";
+type PageProps = { params: { slug: string } };
+
+export default async function BlogPostsPage({ params }: PageProps) {
+  const data = await fs.readFile("data/blog-posts.json", { encoding: "utf-8" });
+  const blogPosts = JSON.parse(data);
+  const blogPost = blogPosts.find((post: any) => post.slug === params.slug);
+
+  if (!blogPost) {
+    return (
+      <main>
+        <span>Blog post does not exist - 404</span>
+      </main>
+    );
+  }
+
+  return (
+    <main className="flex flex-col gap-2 ml-12">
+      <h1 className="text-4xl mt-10 border-b-2">{blogPost.title}</h1>
+      <div className="text-gray-400 flex flex-col">
+        <span>{blogPost.author}</span>
+        <span>{new Date().toUTCString()}</span>
+      </div>
+      <p>{blogPost.content}</p>
+    </main>
+  );
+}
